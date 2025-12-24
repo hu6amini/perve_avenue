1aa9af433113d642c051a758aff668d97a337471
jr <89549621+hu6amini@users.noreply.github.com>
2025-12-24 14:55:32 +0200

    Update dynamic_loader.js

--- dynamic_loader.js
+++ dynamic_loader.js
@@ -68,7 +68,7 @@ document.head.appendChild(instantPagePreload);
                 // Add forum_core_observer.js with same attributes as pa_scripts
                 const forumCoreObserver = document.createElement("script");
                 Object.assign(forumCoreObserver, {
-                    src: "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@3ac7fcc/forum_core_observer.js",
+                    src: "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@07bd83f/forum_core_observer.js",
                     defer: true,
                     crossOrigin: "anonymous",
                     referrerPolicy: "no-referrer"
