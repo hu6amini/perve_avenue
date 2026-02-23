6500f3c1ead05959421f48961a2921e99f2fd4d2
jr <89549621+hu6amini@users.noreply.github.com>
2026-02-23 07:53:05 +0200

    Update dynamic_loader.js

--- dynamic_loader.js
+++ dynamic_loader.js
@@ -116,7 +116,7 @@ document.head.appendChild(instantPagePreload);
                 // Add forum_enhacer.js with same attributes as pa_scripts
                 const forumEnhancer = document.createElement("script");
                 Object.assign(forumEnhancer, {
-                    src: "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@38c352c/forum_enhancer.js",
+                    src: "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e3f97cc/forum_enhancer.js",
                     defer: true,
                     crossOrigin: "anonymous",
                     referrerPolicy: "no-referrer"
