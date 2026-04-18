81b6a06e0d218eecdb8d16af5c6935110a5b2b7a
jr <89549621+hu6amini@users.noreply.github.com>
2026-04-18 18:17:21 +0300

    Update event-bus.js

--- core/event-bus.js
+++ core/event-bus.js
@@ -1,12 +1,11 @@
 // core/event-bus.js
 // Central event communication between modules
-
 var ForumEventBus = (function() {
     'use strict';
-    
+   
     var events = {};
     var debug = false;
-    
+   
     function on(eventName, callback) {
         if (!events[eventName]) {
             events[eventName] = [];
@@ -14,26 +13,26 @@ var ForumEventBus = (function() {
         events[eventName].push(callback);
         if (debug) console.log('[EventBus] Registered:', eventName);
     }
-    
+   
     function off(eventName, callback) {
         if (!events[eventName]) return;
-        
+       
         if (!callback) {
             delete events[eventName];
             if (debug) console.log('[EventBus] Removed all:', eventName);
             return;
         }
-        
+       
         var index = events[eventName].indexOf(callback);
         if (index !== -1) {
             events[eventName].splice(index, 1);
             if (debug) console.log('[EventBus] Removed one from:', eventName);
         }
     }
-    
+   
     function trigger(eventName, data) {
         if (!events[eventName]) return;
-        
+       
         var results = [];
         for (var i = 0; i < events[eventName].length; i++) {
             try {
@@ -43,11 +42,11 @@ var ForumEventBus = (function() {
                 console.error('[EventBus] Error in ' + eventName + ' handler:', e);
             }
         }
-        
+       
         if (debug) console.log('[EventBus] Triggered:', eventName, data);
         return results;
     }
-    
+   
     function once(eventName, callback) {
         var wrapper = function(data) {
             callback(data);
@@ -55,21 +54,21 @@ var ForumEventBus = (function() {
         };
         on(eventName, wrapper);
     }
-    
+   
     function enableDebug() {
         debug = true;
         console.log('[EventBus] Debug mode enabled');
     }
-    
+   
     function disableDebug() {
         debug = false;
     }
-    
+   
     function clear() {
         events = {};
         if (debug) console.log('[EventBus] All events cleared');
     }
-    
+   
     return {
         on: on,
         off: off,
