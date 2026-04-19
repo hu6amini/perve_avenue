28f090d13a21c6e897685303a72108bf3cc719b4
jr <89549621+hu6amini@users.noreply.github.com>
2026-04-19 20:32:56 +0300

    Update dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -78,7 +78,7 @@ document.head.appendChild(instantPagePreload);
             // ============================================
             // 6. MAIN ENHANCER (MUST BE LAST)
             // ============================================
-            "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@f1f8ac2/core/forum-enhancer.js"
+            "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@4e09a28/core/forum-enhancer.js"
         ]);
         
         requestIdleCallback(() => {
