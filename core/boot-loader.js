b1bc942788a100c6466e7fe5c1f005655aa4eeba
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-11 13:56:18 +0300

    Update boot-loader.js

--- core/boot-loader.js
+++ core/boot-loader.js
@@ -7,6 +7,7 @@
         "jq.js",
         "plugin_v3.js",
         "boot-loader.min.js", 
+        "boot-loader.js", 
         "dynamic-loader.min.js",
         "modern-forum.min.css",
         "all.min.css"      
