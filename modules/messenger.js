9df75c56095a22d59d7f19717e1224955164d3cb
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-24 10:26:53 +0300

    Update messenger.js

--- modules/messenger.js
+++ modules/messenger.js
@@ -373,7 +373,7 @@ var MessengerModule = (function(Utils, EventBus) {
         actions.className = 'modern-actions';
         actions.innerHTML = ''
             + '<button type="button" id="modern-preview" class="modern-btn modern-btn-secondary"><i class="fa-regular fa-eye"></i> Preview</button>'
-            + '<button type="button" id="modern-submit" class="modern-btn modern-btn-primary"><i class="fa-regular fa-paper-plane"></i> Send Message</button>';
+            + '<button type="button" id="modern-submit" class="modern-btn modern-btn-primary"><i class="fa-regular fa-paper-plane"></i> Send message</button>';
 
         // Preview area
         var previewArea = document.createElement('div');
