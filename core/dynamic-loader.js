941688cd71ce29acbb61ab7379c28a35e0093c25
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-28 18:51:07 +0300

    Update dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -217,7 +217,7 @@ async function bootSystem() {
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@166baf94c99ec634efacda7561f171ab86ef0b23/modules/posts.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
-                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c79b3e1fe7a6da533e1ef6460a94cd8502df71ad/modules/messenger.js")
+                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@abe42defaeba514b6d6e0f758d7f22ebbaf8c178/modules/messenger.js")
             ]);
 
             const failed = results.filter(r => r.status === 'rejected');
