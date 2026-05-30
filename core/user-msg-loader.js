9324724d13e6c6568df05027adbaeefe21139eeb
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-30 13:01:58 +0300

    Update user-msg-loader.js

--- core/user-msg-loader.js
+++ core/user-msg-loader.js
@@ -231,7 +231,7 @@ whenBodyReady(() => {
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@166baf94c99ec634efacda7561f171ab86ef0b23/modules/posts.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
-                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@fade952ffe9af974a914317b34e98fa5628fd21d/modules/messenger.js")
+                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@bf88abb07f0dddec881ebc11d245946498fbde26/modules/messenger.js")
                 ]);
 
                 const failed = results.filter(r => r.status === 'rejected');
