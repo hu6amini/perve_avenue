26de17595e99df2fab946adbc38a525b71e5cbd6
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-30 11:09:58 +0300

    Update dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -5,7 +5,7 @@ document.documentElement.lang = "en";
 // ============================================================================
 // CONSTANTS
 // ============================================================================
-const CONTENT_PAGE_IDS = Object.freeze(['topic', 'send', 'search', 'blog', 'msg', 'user']);
+const CONTENT_PAGE_IDS = Object.freeze(['topic', 'send', 'search', 'blog']);
 
 const STYLESHEETS = Object.freeze([
     "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css",
@@ -216,8 +216,7 @@ async function bootSystem() {
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c1066340cbf311e771dcbb89968413bd5cb646d2/modules/media-dimensions.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@166baf94c99ec634efacda7561f171ab86ef0b23/modules/posts.min.js"),
-                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
-                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@9d23d20108b84534e21d1e026a278bb651d1e92c/modules/messenger.js")
+                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js")
             ]);
 
             const failed = results.filter(r => r.status === 'rejected');
