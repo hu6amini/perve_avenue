ec5c67e69227bb576c205eed1f9ddc3d10a45f1c
jr <89549621+hu6amini@users.noreply.github.com>
2026-06-08 07:50:41 +0300

    Update posts.min.js script URL in dynamic-loader

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -215,7 +215,7 @@ async function bootSystem() {
             const results = await Promise.allSettled([
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c1066340cbf311e771dcbb89968413bd5cb646d2/modules/media-dimensions.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
-                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@7f59ef06eae641008ad82537352419f078b790bd/modules/posts.min.js"),
+                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@3e639a37a5097354c861862f59147ba6283e8949/modules/posts.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js")
             ]);
 
