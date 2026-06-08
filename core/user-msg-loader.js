72ad4be0c1af1d31c6f3b12b2c22090581d69584
jr <89549621+hu6amini@users.noreply.github.com>
2026-06-08 08:03:51 +0300

    Update script URL for posts.min.js

--- core/user-msg-loader.js
+++ core/user-msg-loader.js
@@ -229,7 +229,7 @@ whenBodyReady(() => {
                 const results = await Promise.allSettled([
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c1066340cbf311e771dcbb89968413bd5cb646d2/modules/media-dimensions.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
-                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@3e639a37a5097354c861862f59147ba6283e8949/modules/posts.min.js"),
+                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@9d46cd8e9675b721e63ed99be6c72318475d7527/modules/posts.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@5ad9b2d52c456c94ec9e8368a8955cf07a24ff09/modules/messenger.js")
                 ]);
