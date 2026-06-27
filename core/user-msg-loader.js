46289cf602061cdb1cee3dc5dc68f58505268144
jr <89549621+hu6amini@users.noreply.github.com>
2026-06-27 16:16:57 +0300

    Update board.js script URL in user-msg-loader

--- core/user-msg-loader.js
+++ core/user-msg-loader.js
@@ -229,7 +229,7 @@ whenBodyReady(() => {
                 const results = await Promise.allSettled([
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@11588f18d3c85bfe9998aea25a0ad1412492c188/modules/media-dimensions.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
-                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c6fd81e2f7b3fd56719c3cf1bdb7308bf3cea113/modules/boards.js"),
+                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e43cbfcdbf420def028854d66e18038a52a2a36d/modules/boards.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@b0f473583279dba7a4c884fced43005a61726b69/modules/posts.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e702d77feba0cae8f5e97dd54412e7ece58ddfba/modules/messenger.js")
