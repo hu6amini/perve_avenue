e40f229c89668c26a4852863d4e807af7eae568a
jr <89549621+hu6amini@users.noreply.github.com>
2026-09-23 21:36:40 +0300

    Update posts.min.js script URL in dynamic-loader

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -218,7 +218,7 @@ async function bootSystem() {
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@3051b903c91564b713f0963e1553a1fde0921e81/modules/breadcrumbs.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@542dedb572fef90b64ec5ea91274ead2e347680d/modules/boards.js"),
-                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@42250f70876a6b2e685361253f94c6841c3aa1a1/modules/posts.min.js"),
+                loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@f0a9ad04a9a42463424848f31846156828e2d8b2/modules/posts.min.js"),
                 loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js")
             ]);
 
