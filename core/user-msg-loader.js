58c2d9c2bd411e4455e9a3540020fd8d65fa5957
jr <89549621+hu6amini@users.noreply.github.com>
2026-09-26 19:35:25 +0300

    Update messenger.js script URL in user-msg-loader

--- core/user-msg-loader.js
+++ core/user-msg-loader.js
@@ -234,7 +234,7 @@ whenBodyReady(() => {
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@542dedb572fef90b64ec5ea91274ead2e347680d/modules/boards.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@0edc62223f2b2578361e7369f2be9a9d21bfd38d/modules/posts.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
-                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@60ec4963afddea0a83785f977eaad9e391046a4c/modules/messenger.js")
+                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@12939be1b688b8315ab770fcd36d1dc1112087ee/modules/messenger.js")
                 ]);
 
                 const failed = results.filter(r => r.status === 'rejected');
